c9f1554bd720cff1b97c8b29214d6bbd38813718
João Ohashi <131549440+Ohashi1409@users.noreply.github.com>
2024-12-16 08:51:48 -0300

    Deletando arquivos que não fazem parte dessa branch

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,35 +1,14 @@
 import { Router } from "express";
-import multer from "multer";
-import uploadConfig from "./config/multer";
-
-import { isAuthenticated } from "./middlewares/isAuthenticated";
-
-// -- Cadastro e login de usuário -- 
-
-import { CreateUserController } from "./controllers/CreateUserController";
-import { AuthUserController } from "./controllers/AuthUserController";
 
 // -- Cadastro de informações bancárias -- 
 
 import { RegisterBankInformationController } from "./controllers/RegisterBankInformationController";
 
-// -- Cadastro e listagem de categorias -- 
-
-import { ListCategoryController } from "./controllers/ListCategoryController";
-
 const router = Router();
-
-// -- Rota de cadastro e login de usuário -- 
-
-router.post("/cadastro", new CreateUserController().handle)
-router.post("/login", new AuthUserController().handle)
 
 // -- Rota de cadastro das informações bancárias --
 
-router.post("/sendBankInfo", isAuthenticated, new RegisterBankInformationController().handle)
+router.post("/sendBankInfo", new RegisterBankInformationController().handle)
 
-// -- Rota de Categorias -- 
-
-router.get("/listCategories", new ListCategoryController().handle)
 
 export { router };
