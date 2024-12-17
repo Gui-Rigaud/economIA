4798096ba20a558cc2690151ae02fbd401d2d99f
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2024-12-17 13:01:40 -0300

    Novo banco de dados com adicao da TABLE Categories

--- backend/prisma/migrations/20241212124831_migration_no_transaction_category/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `categoria_transacao` on the `fin_transactions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `fin_transactions` DROP COLUMN `categoria_transacao`;
