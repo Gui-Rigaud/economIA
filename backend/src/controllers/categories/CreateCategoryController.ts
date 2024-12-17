c455232c8400773ce13a966801e6f8856f5ebc4d
João Ohashi <131549440+Ohashi1409@users.noreply.github.com>
2024-12-16 21:00:59 -0300

    Criando rascunho da rota de categoria

---
+++ backend/src/controllers/categories/CreateCategoryController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { CreateCategoryService } from "../../services/categories/CreateCategoryService";
+
+class CreateCategoryController{
+    async handle(req: Request, res: Response){
+
+        const { user_id, category_name } = req.body;
+
+        const createCategoryService = new CreateCategoryService;
+
+        const category = await createCategoryService.execute(user_id);
+
+        return res.json(category);
+    }
+}
+
+export { CreateCategoryService }
