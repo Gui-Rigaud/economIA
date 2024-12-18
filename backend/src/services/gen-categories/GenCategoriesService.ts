87932723352b7d0a5279073dc396c22c52916749
Gui-Rigaud <guirigaud@hotmail.com>
2024-12-18 15:35:32 -0300

    Atualizando a funcionalidade de gerar categorias para também realizar o update das categorias das transações

--- backend/src/services/gen-categories/GenCategoriesService.ts
+++ backend/src/services/gen-categories/GenCategoriesService.ts
@@ -1,11 +1,16 @@
+import { CategorizeFinTransactionController } from "../../controllers/transactions/CategorizeFinTransactionController";
 import { FileCopier } from "../../utilities/copyFile";
 import { generate } from "../../utilities/vertexai";
+import { CreateCategoryService } from "../categories/CreateCategoryService";
+import { CategorizeFinTransactionService } from "../transactions/CategorizeFinTransactionService";
 
 const fs = require('fs').promises;
 
+const prompt = "Devolva a categoria de cada transação financeira na seguinte fatura de Cartão de Crédito. Além disso, faça a lista de objetos do arquivo JSON com somente o id da transação e a categoria em cada objeto.";
+
 class GenCategoriesService {
 
-    async execute(transactions: any) {
+    async execute(transactions: any, user_id: string) {
 
         const transactionsList = transactions;
 
@@ -13,7 +18,19 @@ class GenCategoriesService {
             await fs.writeFile('/home/guilherme-rigaud/economIA/backend/src/services/gen-categories/fatura_cartao.txt', JSON.stringify(transactionsList, null, 2));
             const copiador = new FileCopier('/home/guilherme-rigaud/economIA/backend/src/services/gen-categories/fatura_cartao.txt');
             await copiador.execute();
-            return generate();
+            const ia_result = await generate(prompt);
+
+            const createCategory = new CreateCategoryService();
+
+            ia_result.map(({id, categoria}) => {
+                createCategory.execute({ category_name: categoria });
+            });
+
+            const categorizeService = new CategorizeFinTransactionService();
+
+            await categorizeService.execute({ transactions_list: ia_result, user_id: user_id });
+
+            return { status: 'ok' };
         } catch (error) {
             console.log(error);
         }
