0440f47eb537f0a318abf0d4416f223150455525
Joao Guilherme Ohashi Ramos <jgor@WINDOWS.CIN.UFPE.BR>
2025-02-19 14:07:52 -0300

    Campo de resumo dos gastos finalizado e testado

--- frontend/src/components/Summary/index.tsx
+++ frontend/src/components/Summary/index.tsx
@@ -44,7 +44,7 @@ export function Summary()
         setLoading(true);
         
         try {
-            const response = await apiClient.get(" ", { 
+            const response = await apiClient.get("/budget", { 
                 params: { user_id: user?.id }
             });
 
@@ -63,12 +63,12 @@ export function Summary()
     };
 
     return (
-        <div id="spendings-summary" className="bg-backgroundLightGray flex flex-col justify-center items-center text-black min-h-screen">
+        <div id="spendings-summary" className="flex flex-col justify-center items-center text-black min-h-screen">
             {primeiraRenderizacao.current ? (
                 <div className="flex items-center justify-center h-screen">
                     <button
                         onClick={fetchSummary}
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-6 rounded-full w-32 h-32 flex items-center justify-center text-2xl"
+                        className="bg-econGreen hover:bg-black text-white font-bold py-6 px-6 rounded-full w-35 h-35 flex items-center justify-center text-2xl"
                     >
                         Resumo de gastos
                     </button>
@@ -80,8 +80,8 @@ export function Summary()
                             <p className="text-white text-xl">Carregando...</p>
                         </div>
                     ) : (
-                        <div id="text-container" className={`bg-gray-200 border-4 border-black rounded-3xl w-3/4 mx-auto flex flex-col justify-center items-center ${roboto400.className} text-[24px]`}>
-                            <p className="mb-4 font-bold"><strong>Resumo de gastos mensais</strong></p>
+                        <div id="text-container" className={`bg-gray-200 border-4 border-black rounded-3xl w-1/3 mx-auto flex flex-col justify-center items-center ${roboto400.className} text-[24px] width-[10px]`}>
+                            <p className="mb-4 mt-3 font-bold"><strong>Resumo de gastos mensais</strong></p>
                             <p className="mb-1 p-4">Receita: {dados?.receita ?? "N/A"}</p>
                             <p className="mb-1 p-4">Despesas totais: {dados?.despesas ?? "N/A"}</p>
                             <p className="mb-1 p-4">Saldo: {dados?.saldo ?? "N/A"}</p>
