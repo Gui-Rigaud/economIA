67231724b3d4a541c23bff167c9487a81f2bdfad
hugoniceas <niceasoriginal@gmail.com>
2025-02-18 21:01:49 -0300

    minor change in the suggestion prompt

--- backend/src/services/suggestion/GenSuggestionsService.ts
+++ backend/src/services/suggestion/GenSuggestionsService.ts
@@ -1,12 +1,12 @@
 import { generate } from '../../utilities/vertexai';
 
-const prompt = "A partir da seguinte fatura de Cartão de Crédito, devolva sugestões e recomendações de gastos não intrusivas, que busquem auxiliar o usuário a ter um maior controle financeiro. Dê respostas em pequenas frases e armazene-as em um JSON contendo apenas as frases e uma numeração para separá-las.";
+const prompt = "A partir da seguinte lista de transações, devolva sugestões e recomendações de gastos não intrusivas, que busquem auxiliar o usuário a ter um maior controle financeiro. Dê respostas em pequenas frases e armazene-as em um JSON contendo apenas as frases e uma numeração para separá-las.";
 
 class GenSuggestionService {
     
     async execute() {
         try {
-            const ia_result = await generate(prompt, 'fatura_cartao.txt');
+            const ia_result = await generate(prompt, 'list_transactions.txt');
 
             return ia_result;
         } catch (error) {
