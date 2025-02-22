1a5e7537abce86ec9a7f2b262d309b7c7cd70290
João Ohashi <131549440+Ohashi1409@users.noreply.github.com>
2025-02-22 11:39:09 -0300

    Andamento da estilização do dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,19 +1,15 @@
 'use client';
 
-import { UserGuide } from "@/components/UserGuide";
 import { Categorias } from "@/components/Categorias";
 import { Summary } from "@/components/Summary";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Suggestions } from "@/components/Suggestions";
-import SideBar from "@/components/Sidebar";
 
 export default function Dashboard() {
   return (
     <AuthProvider>
       <div className="flex h-screen">
-        <SideBar />
-        <div className="flex-grow">
-          <UserGuide />
+        <div className="flex-grow mt-[-320px]">
           <Categorias />
           <Summary />
           <Suggestions />
