05d692d0894cd8e1ef5b137ed26ee9c6d8d9e5bb
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-03-15 15:52:32 -0300

    Adição da rota de geração de categorias por PDF

---
+++ backend/src/controllers/gen-categories/GenCategoriesControllerPDF.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { GenCategoriesServicePDF } from "../../services/gen-categories/GenCategoriesServicePDF";
+
+class GenCategoriesControllerPDF {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const genCategoriesService = new GenCategoriesServicePDF();
+        const { user_id } = request.body;
+
+        try {
+            const categories = await genCategoriesService.execute(user_id); // 🔹 Remove file.buffer
+            return response.json(categories);
+        } catch (error) {
+            return response.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+}
+
+export { GenCategoriesControllerPDF };
