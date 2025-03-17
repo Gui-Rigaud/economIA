648081103085192830839905ef4c53ca9ff7af0f
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-03-17 17:08:56 -0300

    Mudança para armazenagem no bucket da IA no próprio arquivo, em vez de uso de arquivo auxiliar

--- backend/src/services/gen-categories/PercentCategoriesService.ts
+++ backend/src/services/gen-categories/PercentCategoriesService.ts
@@ -1,7 +1,8 @@
-import { FileCopier } from "../../utilities/copyFile";
-import { generate } from "../../utilities/vertexai";
+import { generate } from "../../utilities/vertexaiTxt";
 
 const fs = require('fs').promises;
+const path = require("path");
+const { Storage: GoogleCloudStorage } = require("@google-cloud/storage");
 
 const prompt2 = "Devolva um JSON com somente a frequência em porcentagem de cada categoria e o nome da categoria. No JSON, a categoria tem que ter o nome 'categoria' e a frequência tem que ter o nome 'porcentagem', ambas sem as aspas.";
 
@@ -10,13 +11,16 @@ class PercentCategoriesService {
     async execute(transactions: any) {
 
         const transactionsList = transactions;
+        const filePath = path.join(__dirname, "list_transactions.txt");
 
         try {
-            const filePath = `${__dirname}/list_transactions.txt`;
             await fs.writeFile(filePath, JSON.stringify(transactionsList, null, 2));
-            const copiador = new FileCopier(filePath);
-            await copiador.execute('list_transactions.txt');
-            await fs.unlink(filePath); // Delete the file
+            const storage = new GoogleCloudStorage();
+            const bucketName = "fatura_cartao_1";
+            const destinationPath = "json/list_transactions.txt";
+            const bucket = storage.bucket(bucketName);
+            await bucket.upload(filePath, { destination: destinationPath });
+            await fs.unlink(filePath);
             return generate(prompt2, 'list_transactions.txt');
         } catch (error) {
             console.log(error); 
