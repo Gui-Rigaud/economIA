321d80671e88ed36321f4ec02b0a6f3da86da346
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-03-19 00:46:12 -0300

    Adição do recebimento de 'user_id'

--- backend/src/controllers/gen-categories/GenCategoriesController.ts
+++ backend/src/controllers/gen-categories/GenCategoriesController.ts
@@ -3,10 +3,13 @@ import { GenCategoriesService } from "../../services/gen-categories/GenCategorie
 
 class GenCategoriesController {
     async handle(request: Request, response: Response): Promise<Response> {
+        const { user_id } = request.body;
+        console.log("User ID recebido: ", user_id);
         const genCategoriesService = new GenCategoriesService();
 
+
         try {
-            const categories = await genCategoriesService.execute();
+            const categories = await genCategoriesService.execute(user_id);
             return response.json(categories);
         } catch (error) {
             return response.status(500).json({ error: "Internal Server Error" });
