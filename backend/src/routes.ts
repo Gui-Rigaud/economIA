7bf9f83a756cbc59eff311597a26af4b202f7a87
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-03-19 08:43:31 -0300

    Remoção das rotas unitilizadas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,12 +11,9 @@ import { RegisterBankInformationController } from "./controllers/user/RegisterBa
 import { RegisterTransactionController } from "./controllers/transactions/RegisterTransactionController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
-import { ListTransactionsController } from "./controllers/transactions/ListTransactionsController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { PercentCategoriesController } from "./controllers/gen-categories/PercentCategoriesController";
 
-import { CreateCategoryController } from "./controllers/categories/CreateCategoryController";
-import { CategorizeFinTransactionController } from "./controllers/transactions/CategorizeFinTransactionController";
 import { BudgetUserController } from "./controllers/user/BudgetUserController";
 
 import { SuggestionController } from "./controllers/suggestion/SuggestionController";
@@ -39,16 +36,12 @@ router.get('/budget', isAuthenticated, new BudgetUserController().handle);
 // --ROTAS TRANSACTION--
 
 
-/* analisar de precisamos*/ router.get('/list/transactions', isAuthenticated, new ListTransactionsController().handle)
 
 router.post('/register/file', isAuthenticated, multerConfig.single('file'), new RegisterTransactionController().handle)
 
 router.post('/generate-categories', isAuthenticated, new GenCategoriesController().handle)
 // -- ROTAS CATEGORIES -- 
 
-router.post("/categories/create", isAuthenticated, new CreateCategoryController().handle)
-
-router.put("/transactions/update", isAuthenticated, new CategorizeFinTransactionController().handle)
 
 router.post("/percent-categories", isAuthenticated, new PercentCategoriesController().handle);
 
