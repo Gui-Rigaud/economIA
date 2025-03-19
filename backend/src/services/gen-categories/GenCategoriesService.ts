afd015c535c8f6fe20cf0b35ca712961807f8ac5
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-03-19 00:57:12 -0300

    Mudança de prompt, checagem se arquivo já foi lido antes, despesas atualizadas no próprio arquivo, deleção dos dados de categories de um usuário caso estejam em uso e criação de categories no banco com os dados retornados pela IA

--- backend/src/services/gen-categories/GenCategoriesService.ts
+++ backend/src/services/gen-categories/GenCategoriesService.ts
@@ -1,13 +1,25 @@
+import prismaClient from "../../prisma";
 import { generate } from "../../utilities/vertexai";
 
 const fs = require("fs").promises;
 const path = require("path");
 const { Storage: GoogleCloudStorage } = require("@google-cloud/storage");
 
-const prompt = "Classifique cada transação financeira desta fatura de Cartão de Crédito em categorias amplas e bem definidas, agrupando categorias semelhantes sob um mesmo rótulo. Utilize supernichos para unificar categorias relacionadas. Por exemplo, 'Restaurante' e 'Alimentação' devem ser classificadas apenas como 'Alimentação', e 'Esportes' e 'Lazer' podem ser unificadas se fizer sentidoRetorne um JSON onde cada objeto contenha apenas dois atributos: 'id' (numeração sequencial crescente a partir de 1) e 'categoria' (o nome do supernicho identificado para a transação)."
+const prompt = 'Classifique cada transação financeira desta fatura de Cartão de Crédito em categorias amplas e bem definidas, agrupando categorias semelhantes sob um mesmo rótulo. Utilize supernichos para unificar categorias relacionadas. Por exemplo, "Restaurante" e "Alimentação" devem ser classificadas apenas como "Alimentação", e "Esportes" e "Lazer" podem ser unificadas se fizer sentido. Retorne um JSON onde cada objeto contenha apenas três atributos: "id" (numeração sequencial crescente a partir de 1), "nome" (o nome do supernicho identificado para a transação) e "valor" contendo o valor da transação. Além disso adicione no final do JSON um objeto no formato { "despesas": valor }, sendo "valor" o total dos valores positivos da fatura.'
 
 class GenCategoriesService {
-    async execute() {
+    async execute(user_id: string) {
+        const consulta = await prismaClient.user.findUnique({
+            where: {
+                id: user_id,
+            },
+            select: {
+                fileBool: true,
+            }
+        })
+        const resultadoConsulta = consulta?.fileBool;
+
+        if (resultadoConsulta){
         const filePath = path.join(__dirname, "fatura.pdf");
         const storage = new GoogleCloudStorage();
         const bucketName = "fatura_cartao_1";
@@ -28,15 +40,56 @@ class GenCategoriesService {
             await new Promise(resolve => setTimeout(resolve, 3000)); 
 
             const ia_result = await generate(prompt, "fatura.pdf");
+            const { despesas } = ia_result.pop()
+            console.log("Despesas: ", despesas)
+            const userData = await prismaClient.user.findUnique({
+                where: { 
+                    id: user_id
+                },
+                select: { 
+                    receita: true,
+                    despesa:true,
+                },
+            });
+            const receita = Number(userData?.receita) || 0;
+            await prismaClient.user.update({
+                where: { 
+                    id: user_id 
+                },
+                data: {
+                    saldo: receita - despesas, 
+                    despesa: despesas,
+                },
+            });
             await fs.unlink(filePath);
             console.log("Arquivo deletado");
-
+            await prismaClient.categories.deleteMany({
+                where: {
+                    user_id: user_id,
+                }
+            });
+            await prismaClient.categories.createMany({
+                data: ia_result.map(categoria => ({
+                    nome: categoria.nome,
+                    valor: Number(categoria.valor), // Certifica que o valor é numérico
+                    user_id: user_id, // Adiciona o user_id como chave estrangeira
+                })),
+            });
             return ia_result;
         } catch (error) {
             console.log(error);
             throw new Error("Erro ao gerar categorias");
         }
+    } else {
+        const ia_result = await prismaClient.categories.findMany();
+        const ia_result_formatada = ia_result.map(categoria => ({
+            id: categoria.id,
+            nome: categoria.nome,
+            valor: Number(categoria.valor), // Converte para número
+        }));
+        return ia_result_formatada;
     }
+}
 }
 
 export { GenCategoriesService };
