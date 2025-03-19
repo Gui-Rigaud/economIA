afb9bedb2ecd83cc0759314f1c385cc48e3fa1f1
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-03-19 00:47:28 -0300

    Adição do recebimento de 'user_id'

--- backend/src/controllers/transactions/RegisterTransactionController.ts
+++ backend/src/controllers/transactions/RegisterTransactionController.ts
@@ -4,6 +4,7 @@ import { RegisterTransactionService } from "../../services/transactions/Register
 class RegisterTransactionController {
     async handle(req: Request, res: Response) {
         const file = req.file;
+        const { user_id } = req.body;
 
         if (!file) {
             return res.status(400).json({ error: "File is required" });
@@ -13,7 +14,7 @@ class RegisterTransactionController {
 
         try {
             // Passa o buffer para o service
-            const saveFile = await registerBankTransactionService.saveFile(file.buffer, file.originalname);
+            const saveFile = await registerBankTransactionService.saveFile(user_id, file.buffer, file.originalname);
             return res.json(saveFile);
         } catch (error) {
             return res.status(500).json({ error: error.message });
