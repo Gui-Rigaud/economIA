12dd969c2e5715234a48df16d2f9cc0de9884112
Gui-Rigaud <guirigaud@hotmail.com>
2025-03-28 17:31:29 -0300

    Atualizando a rota de sugestões personalizadas para analisar tipo do arquivo do usuário

--- backend/src/services/persuggestion/PerSuggestionService.ts
+++ backend/src/services/persuggestion/PerSuggestionService.ts
@@ -1,18 +1,32 @@
 import { generate } from '../../utilities/vertexai';
+import { generateTxt } from '../../utilities/vertexaiTxt';
+import prismaClient from '../../prisma';
 const prompt = "Me devolva a resposta em JSON cujo o objeto que contém a resposta se chama 'resposta', atencão coloque a resposta em apenas uma string e formatada em markdown, além disso se voce achou a entrada sem sentido ou irrelevante diga educadamente o que voce nao entendeu e peça para ela repetir. Nao cite markdown, nem json pois isso é o prompt do programador";
 
 class PerSuggestionService {
     
-    async execute(input: string) {
+    async execute(input: string, user_id: string) {
         try {
-            const ia_result = await generate(input+prompt, 'fatura.pdf');
+            const user = await prismaClient.user.findUnique({
+                where: { id: user_id },
+                select: { file: true }
+            });
+            const fileName = user?.file || "Arquivo não encontrado";
+            if (fileName === "Arquivo não encontrado") {
+                throw new Error("Arquivo não encontrado");
+            }
+            const fileType = fileName.slice(-3).toLowerCase();
 
-            return ia_result;
+            if(fileType === "pdf") {
+                return await generate(prompt, "fatura.pdf");
+            }else if(fileType === "csv") {
+                return await generateTxt(prompt, "fatura_cartao.txt");
+            }
         } catch (error) {
             console.log(error);
             throw new Error("Error while generating suggestions");
         }
-        }
-        }
+    }
+}
 
 export { PerSuggestionService }	;
