ee17b456278a84e634276a7e798defd756c04403
Gui-Rigaud <guirigaud@hotmail.com>
2025-03-28 17:27:46 -0300

    Adicionando o user_id a ser recebido pelo body

--- backend/src/controllers/persuggestion/PerSuggestionController.ts
+++ backend/src/controllers/persuggestion/PerSuggestionController.ts
@@ -3,15 +3,19 @@ import { PerSuggestionService } from '../../services/persuggestion/PerSuggestion
 
 class PerSuggestionController {
   async handle(req: Request, res: Response) {
-    const { input } = req.body;
+    const { input, user_id } = req.body;
     
     if (!input) {
       return res.status(400).json({ error: "Input é obrigatório" });
     }
     
+    if (!user_id) {
+      return res.status(400).json({ error: "user_id é obrigatório" });
+    }
+
     try {
       const perSuggestionService = new PerSuggestionService();// PerSuggestionService 
-      const suggestions = await perSuggestionService.execute(input);
+      const suggestions = await perSuggestionService.execute(input, user_id);
       
       return res.json({ suggestion: suggestions });
     } catch (error) {
